2b6542e9ae37fb36390045128e560e7552538df1
Kaitlyn Higgins <kaitlynhiggins@Kaitlyns-MacBook-Pro.local>
2019-06-15 18:53:22 -0500

    Creates movie_vue_app, congigures CRUD for Actors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,114 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h2>Add New Actor</h2>
+    <div>
+      First Name: <input type="text" v-model="newActorFirstName"><br>
+      Last Name: <input type="text" v-model="newActorLastName"><br>
+      Known For: <input type="text" v-model="newActorKnownFor"><br>
+      Gender: <input type="text" v-model="newActorGender"><br>
+      Age: <input type="text" v-model="newActorAge"><br>
+      Movie Id: <input type="number" v-model="newActorMovieId"><br>
+      <button v-on:click="createActor()">Create Actor</button>
+    </div>
+
+    <h1>{{ message }}</h1>
+    <div v-for="actor in actors">
+      <h2>{{ actor.first_name }} {{ actor.last_name }}</h2>
+      <button v-on:click="showActor(actor)">See More</button>
+      <div v-if="currentActor === actor">
+      <p>Known for: {{ actor.known_for }}</p>
+      <p>Gender: {{ actor.gender }}</p>
+      <p>Age: {{ actor.age }}</p>
+      
+      <div>
+        First Name: <input type="text" v-model="actor.first_name"><br>
+        Last Name: <input type="text" v-model="actor.last_name"><br>
+        Known For: <input type="text" v-model="actor.known_for"><br>
+        Gender: <input type="text" v-model="actor.gender"><br>
+        Age: <input type="text" v-model="actor.age"><br>
+        Movie Id: <input type="text" v-model="actor.movie_id"><br>
+        <button v-on:click="updateActor(actor)">Update Actor</button><br>
+        <button v-on:click="destroyActor(actor)">Remove Actor</button>
+      </div>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Actors",
+      actors: [],
+      newActorFirstName: "",
+      newActorLastName: "",
+      newActorKnownFor: "",
+      newActorGender: "",
+      newActorAge: "",
+      newActorMovieId: "",
+      currentActor: {}
+    };
+  },
+  created: function() {
+    axios.get("/api/actors").then(response => {
+      this.actors = response.data;
+    });
+  },
+  methods: {
+    createActor: function() {
+      var params = {
+        first_name: this.newActorFirstName,
+        last_name: this.newActorLastName,
+        known_for: this.newActorKnownFor,
+        gender: this.newActorGender,
+        age: this.newActorAge,
+        movie_id: this.newActorMovieId
+      };
+      axios.post("/api/actors", params).then(response => {
+        console.log(response);
+        this.actors.push(response.data);
+        this.newActorFirstName = "";
+        this.newActorLastName = "";
+        this.newActorKnownFor = "";
+        this.newActorGender = "";
+        this.newActorAge = "";
+        this.newActorMovieId = "";
+      });
+    },
+    showActor: function(actor) {
+      if (this.currentActor === actor) {
+        this.currentActor = {};
+      } else {
+        this.currentActor = actor;
+      }
+    },
+    updateActor: function(actor) {
+      var params = {
+        first_name: actor.first_name,
+        width: actor.last_name,
+        known_for: actor.known_for,
+        gender: actor.gender,
+        age: actor.age,
+        movie_id: actor.movie_id
+      };
+      axios.patch("/api/actors/" + actor.id, params).then(response => {
+        this.currentActor = {};
+      });
+    },
+    destroyActor: function(actor) {
+      axios.delete("/api/actors/" + actor.id).then(response => {
+        var index = this.actors.indexOf(actor);
+        this.actors.splice(index, 1);
+      });
+    }
   }
-}
+};
 </script>
