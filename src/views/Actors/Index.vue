776631c1c573d786835afd6213d4e7e05d597d5d
Kaitlyn Higgins <kaitlynhiggins@Kaitlyns-MacBook-Pro.local>
2019-06-21 22:32:46 -0500

    Adds filtering, sorting and autocomplete to indexes for movies and actors

--- src/views/Actors/Index.vue
+++ src/views/Actors/Index.vue
@@ -21,81 +21,61 @@
          </div>
     </section>
 
+    <!-- search bar -->
+
+    <h3>
+      <label for="name">Search by name: </label>
+      <input type="text" id="name" placeholder="enter name" v-model="nameFilter" list="names">
+    </h3>
+
+    <!-- autocomplete -->
+
+    <datalist id="names">
+      <option v-for="actor in actors">{{ actor.first_name }} {{ actor.last_name }}</option>
+    </datalist>
+
+
+    <!-- order by button -->
+
+    <div>
+      <button class="smoothScroll btn btn-default" v-on:click="setSortAttribute('first_name')">Sort by first name</button>
+
+      <i v-if="sortAttribute === 'first_name' && sortAscending === 1" class="icon-arrow-up"></i>
+      <i v-if="sortAttribute === 'first_name' && sortAscending === -1" class="icon-arrow-down"></i>
+
+
+      <button class="smoothScroll btn btn-default" v-on:click="setSortAttribute('last_name')">Sort by last name</button>
+
+      <i v-if="sortAttribute === 'last_name' && sortAscending === 1" class="icon-arrow-up"></i>
+      <i v-if="sortAttribute === 'last_name' && sortAscending === -1" class="icon-arrow-down"></i>
+
+
+    </div>
+
+    <!-- main content -->
+
     <section id="about">
          <div class="container">
               <div class="row">
 
                    <div class="col-md-offset-1 col-md-10 col-sm-12">
-                        <div class="col-md-4 col-sm-4" v-for="actor in actors">
+                    
+                        <div class="col-md-4 col-sm-4" v-for="actor in orderBy(filterBy(actors, nameFilter, 'first_name', 'last_name'), sortAttribute, sortAscending)">
+
+
                              <h2>{{ actor.first_name }} {{ actor.last_name }}</h2>
                              <p>Known For: <strong>{{ actor.known_for }}</strong></p>
-<!--                              <p>Actor Genres: <strong>{{ actor.genres }}</strong></p>
- -->                             <router-link v-bind:to="'/actors/' + actor.id">
+                            <router-link v-bind:to="'/actors/' + actor.id">
                                <button class="smoothScroll btn btn-default">More Info</button>
                              </router-link>
                         </div>
 
-                       <!--  <div class="col-md-4 col-sm-4">
-                             <h2>New Design</h2>
-                             <p>Quisque vitae felis eros. Suspendisse quis leo molestie, iaculis sapien id, hendrerit augue. Phasellus fermentum.</p>
-                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit Ut enim ad minim veniam, quis nostrud exercitation.</p>
-                        </div>
-
-                        <div class="col-md-4 col-sm-4">
-                             <h2>HTML CSS</h2>
-                             <p>Vivamus non scelerisque ex, et interdum leo. In bibendum lacus vitae felis egestas, at consectetur metus facilisis.</p>
-                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit Ut enim ad minim veniam, quis nostrud exercitation.</p>
-                        </div>
-
-                        <div class="col-md-6 col-sm-6">
-                             <h3>Fully Customizable!</h3>
-                             <p>Maecenas quis pulvinar neque, non dapibus orci. Integer non suscipit dui. In eu tempor sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras porta lacus ac sagittis imperdiet. Vestibulum eget mattis quam.</p>
-                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercita.</p>
-                        </div>
-
-                        <div class="col-md-6 col-sm-6">
-                             <img src="images/about-image.jpg" class="img-responsive" alt="About Image">
-                        </div>
-
-                        <div class="clearfix"></div>
-
-                        <div class="col-md-4 col-sm-4">
-                             <img src="images/about-image1.jpg" class="img-responsive" alt="Blog Image">
-                             <h3>Section One</h3>
-                             <p>Pellentesque lobortis velit mi, sed volutpat enim facilisis at. Maecenas quis pulvinar neque, non dapibus orci.</p>
-                        </div>
-
-                        <div class="col-md-4 col-sm-4">
-                             <img src="images/about-image2.jpg" class="img-responsive" alt="Blog Image">
-                             <h3>Section Two</h3>
-                             <p>Pellentesque lobortis velit mi, sed volutpat enim facilisis at. Maecenas quis pulvinar neque, non dapibus orci.</p>
-                        </div>
-
-                        <div class="col-md-4 col-sm-4">
-                             <img src="images/about-image3.jpg" class="img-responsive" alt="Blog Image">
-                             <h3>Section Three</h3>
-                             <p>Pellentesque lobortis velit mi, sed volutpat enim facilisis at. Maecenas quis pulvinar neque, non dapibus orci.</p>
-                        </div>
-              
-                        <h3>One Column</h3>
-                        <p>Fusce finibus ac orci ut feugiat. Duis accumsan dui non augue ullamcorper, at porta elit lacinia. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras at est non quam mattis hendrerit. Fusce purus massa, pretium malesuada eros nec, feugiat iaculis sem.</p> -->
+                       
                    </div>
 
               </div>
          </div>
     </section>
-
-    <!-- <h1>Actors</h1>
-    <div v-for="actor in actors">
-      <h2>{{ actor.first_name }} {{ actor.last_name }}</h2>
-      <p>Known for: {{ actor.known_for }}</p>
-      <p>actor Genres: {{ actor.genres }}</p>
-    
-    <router-link v-bind:to="'/actors/' + actor.id">
-      <button>More Info</button>
-    </router-link>
-
-    </div> -->
     
 
   </div>
@@ -103,13 +83,18 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function() {
     return {
       actors: [],
       actor: {},
-      errors: []
+      errors: [],
+      nameFilter: '',
+      sortAttribute: "",
+      sortAscending: 1
     };
   },
   created: function() {
@@ -120,5 +105,15 @@ export default {
       this.errors = error.response.data.errors;
     });
   },
+  methods: {
+    setSortAttribute: function(attribute) {
+      if (this.sortAttribute === attribute) {
+        this.sortAscending = this.sortAscending * -1;
+      } else {
+        this.sortAscending = 1;
+      }
+      this.sortAttribute = attribute;
+    }
+  }
 };
 </script>
--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -22,14 +22,46 @@
     </section>
 
 
-    <!-- About Section -->
+    <!-- search bar -->
+
+    <h3>
+      <label for="title">Search by title: </label>
+      <input type="text" id="title" placeholder="enter title" v-model="titleFilter" list="titles">
+    </h3>
+
+    <!-- autocomplete -->
+
+    <datalist id="titles">
+      <option v-for="movie in movies">{{ movie.title }}</option>
+    </datalist>
+
+
+    <!-- order by button -->
+
+    <div>
+      <button class="smoothScroll btn btn-default" v-on:click="setSortAttribute('title')">Sort by title</button>
+
+      <i v-if="sortAttribute === 'title' && sortAscending === 1" class="icon-arrow-up"></i>
+      <i v-if="sortAttribute === 'title' && sortAscending === -1" class="icon-arrow-down"></i>
+
+
+      <button class="smoothScroll btn btn-default" v-on:click="setSortAttribute('year')">Sort by year released</button>
+
+      <i v-if="sortAttribute === 'year' && sortAscending === 1" class="icon-arrow-up"></i>
+      <i v-if="sortAttribute === 'year' && sortAscending === -1" class="icon-arrow-down"></i>
+
+    </div>
+
+    <!-- Main content -->
 
     <section id="about">
          <div class="container">
               <div class="row">
 
-                   <div class="col-md-offset-1 col-md-10 col-sm-12">
-                        <div class="col-md-4 col-sm-4" v-for="movie in movies">
+                   <transition-group appear enter-active-class="animated fadeIn" leave-active-class="animated bounce" class="col-md-offset-1 col-md-10 col-sm-12">
+
+                        <div class="col-md-4 col-sm-4" v-for="movie in orderBy(filterBy(movies, titleFilter, 'title'), sortAttribute, sortAscending)" v-bind:key="movie.id">
+
                              <h2>{{ movie.title }}</h2>
                              <p>Year released: <strong>{{ movie.year }}</strong></p>
                              <p>Movie Genres: <strong>{{ movie.genres }}</strong></p>
@@ -38,82 +70,30 @@
                              </router-link>
                         </div>
 
-                       <!--  <div class="col-md-4 col-sm-4">
-                             <h2>New Design</h2>
-                             <p>Quisque vitae felis eros. Suspendisse quis leo molestie, iaculis sapien id, hendrerit augue. Phasellus fermentum.</p>
-                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit Ut enim ad minim veniam, quis nostrud exercitation.</p>
-                        </div>
-
-                        <div class="col-md-4 col-sm-4">
-                             <h2>HTML CSS</h2>
-                             <p>Vivamus non scelerisque ex, et interdum leo. In bibendum lacus vitae felis egestas, at consectetur metus facilisis.</p>
-                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit Ut enim ad minim veniam, quis nostrud exercitation.</p>
-                        </div>
-
-                        <div class="col-md-6 col-sm-6">
-                             <h3>Fully Customizable!</h3>
-                             <p>Maecenas quis pulvinar neque, non dapibus orci. Integer non suscipit dui. In eu tempor sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras porta lacus ac sagittis imperdiet. Vestibulum eget mattis quam.</p>
-                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercita.</p>
-                        </div>
-
-                        <div class="col-md-6 col-sm-6">
-                             <img src="images/about-image.jpg" class="img-responsive" alt="About Image">
-                        </div>
-
-                        <div class="clearfix"></div>
-
-                        <div class="col-md-4 col-sm-4">
-                             <img src="images/about-image1.jpg" class="img-responsive" alt="Blog Image">
-                             <h3>Section One</h3>
-                             <p>Pellentesque lobortis velit mi, sed volutpat enim facilisis at. Maecenas quis pulvinar neque, non dapibus orci.</p>
-                        </div>
-
-                        <div class="col-md-4 col-sm-4">
-                             <img src="images/about-image2.jpg" class="img-responsive" alt="Blog Image">
-                             <h3>Section Two</h3>
-                             <p>Pellentesque lobortis velit mi, sed volutpat enim facilisis at. Maecenas quis pulvinar neque, non dapibus orci.</p>
-                        </div>
-
-                        <div class="col-md-4 col-sm-4">
-                             <img src="images/about-image3.jpg" class="img-responsive" alt="Blog Image">
-                             <h3>Section Three</h3>
-                             <p>Pellentesque lobortis velit mi, sed volutpat enim facilisis at. Maecenas quis pulvinar neque, non dapibus orci.</p>
-                        </div>
-              
-                        <h3>One Column</h3>
-                        <p>Fusce finibus ac orci ut feugiat. Duis accumsan dui non augue ullamcorper, at porta elit lacinia. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras at est non quam mattis hendrerit. Fusce purus massa, pretium malesuada eros nec, feugiat iaculis sem.</p> -->
-                   </div>
+                   </transition-group>
 
               </div>
          </div>
     </section>
-    <!-- <div v-for="movie in movies">
-      <h2>{{ movie.title }}</h2>
-      <p>Year released: {{ movie.year }}</p>
-      <p>Plot: {{ movie.plot }}</p>
-      <p>Directed by: {{ movie.director }}</p>
-      <p>In English: {{ movie.english }}</p>
-      <p>Movie Genres: {{ movie.genres }}</p>
-    
-    <router-link v-bind:to="'/movies/' + movie.id">
-      <button>More Info</button>
-    </router-link>
-
-    </div> -->
-    
 
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
+
 
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function() {
     return {
       movies: [],
       movie: {},
-      errors: []
+      errors: [],
+      titleFilter: '',
+      sortAttribute: "",
+      sortAscending: 1
     };
   },
   created: function() {
@@ -124,5 +104,16 @@ export default {
       this.errors = error.response.data.errors;
     });
   },
+
+  methods: {
+    setSortAttribute: function(attribute) {
+      if (this.sortAttribute === attribute) {
+        this.sortAscending = this.sortAscending * -1;
+      } else {
+        this.sortAscending = 1;
+      }
+      this.sortAttribute = attribute;
+    }
+  }
 };
 </script>
